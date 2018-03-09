fe2f3264a475dab969c0e5fab9bb7b763ae44a4b
Shine <ShineSmile@outlook.com>
2018-03-09 13:31:28 +0800

    Add a unit test for read config file when it's missing.

--- Scavenger/StreamUtility.cs
+++ Scavenger/StreamUtility.cs
@@ -1,9 +1,16 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 namespace Scavenger
 {
-    internal static class StreamUtility
+    public static class StreamUtility
     {
+        /// <summary>
+        /// Read stream as string.
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <exception cref="ArgumentException">Stream will close after reader dispose. Don't call it MORE THAN ONCE for same stream.</exception>
+        /// <returns>stream content as string</returns>
         public static string ReadToEnd(this Stream stream)
         {
             using (var reader = new StreamReader(stream))
@@ -12,12 +19,17 @@ namespace Scavenger
             }
         }
 
+        /// <summary>
+        /// An extend for writing string to stream.
+        /// </summary>
+        /// <param name="stream">the stream be writed into string</param>
+        /// <param name="value">string for writing to the stream</param>
+        /// <exception cref="ArgumentException">Stream will close after writer dispose. Don't call it MORE THAN ONCE for same stream.</exception>
         public static void Write(this Stream stream, string value)
         {
             using (var writer = new StreamWriter(stream))
             {
                 writer.Write(value);
-                writer.Flush();
             }
         }
     }
